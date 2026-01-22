9c802da5ce964753aef0c877629df55c21d822b0
vantaylo <vanessalyn.taylor@gmail.com>
2026-01-21 18:46:45 -0600

    create navbar compontent

---
+++ SafeStep/app/Components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div style={NavbarContainerStyle}>
+      <h1 style={navbarTitleStyle}>SafeStep</h1>
+    </div>
+  );
+}
+
+const NavbarContainerStyle = {
+  width: '100%',
+  height: '60px',
+  backgroundColor: '#000000',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const navbarTitleStyle = {
+  color: '#FFFFFF',
+  margin: 0,
+  fontSize: '24px',
+};
