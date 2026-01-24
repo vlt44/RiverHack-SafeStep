e85c863ccf77898b9bbb16fdb0ceba2e0b85a5f8
vantaylo <vanessalyn.taylor@gmail.com>
2026-01-23 22:34:23 -0600

    deleted unused vars

--- SafeStep/app/_layout.tsx
+++ SafeStep/app/_layout.tsx
@@ -1,13 +1,12 @@
 import { View } from 'react-native';
-import mockData from "./MockData/mock.json";
 import Navbar from './Components/Navbar';
 import IntersectionDisplay from './Components/IntersectionDisplay';
 
 export default function HomeScreen() {
   return (
     <View>
-      <Navbar />
-      <IntersectionDisplay data={mockData} />
+      <Navbar/>
+      <IntersectionDisplay/>
     </View>
   )
 }
