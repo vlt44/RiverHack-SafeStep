42751cf61cdbc7cc9073983f56248823a86c536f
vantaylo <vanessalyn.taylor@gmail.com>
2026-01-23 19:32:17 -0600

    cleaned up navbar

--- SafeStep/app/Components/Navbar.tsx
+++ SafeStep/app/Components/Navbar.tsx
@@ -2,23 +2,20 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div style={NavbarContainerStyle}>
-      <div style={navbarTitleStyle}>SafeStep</div>
+    <div style={NavbarStyling}>
+      SafeStep
     </div>
   );
 }
 
-const NavbarContainerStyle = {
+const NavbarStyling = {
   width: '100%',
   height: '60px',
   backgroundColor: '#000000',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-}
-
-const navbarTitleStyle = {
   color: '#FFFFFF',
   margin: 0,
   fontSize: '24px',
-};
+}
