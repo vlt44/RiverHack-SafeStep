b16169a53f703fc04152d4e799a160b4473f47c2
vantaylo <vanessalyn.taylor@gmail.com>
2026-03-06 19:36:09 -0600

    setup testing file for display

---
+++ SafeStep/app/Components/IntersectionDisplay.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, screen } from "@testing-library/react-native";
+import IntersectionDisplay from "./IntersectionDisplay";
+import { SIGNAL_STATES } from "";
+
+describe("IntersectionDisplay", () => {
+  test("renders display component", () => {});
+
+  test("displays intersection name", () => {});
+
+  test("displays signal status", () => {});
+
+  test("renders signal icon", () => {});
+
+  test("has correct accessibility label", () => {});
+});
